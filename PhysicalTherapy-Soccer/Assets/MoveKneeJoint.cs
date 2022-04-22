ea9f49f70c0edf9d11bc5696dd136cbc3f8dc569
Jeremy Young <young.a.jeremy@gmail.com>
2022-04-22 17:33:59 -0600

    Fixed leg positioning

--- PhysicalTherapy-Soccer/Assets/MoveKneeJoint.cs
+++ PhysicalTherapy-Soccer/Assets/MoveKneeJoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveKneeJoint : MonoBehaviour
 {
     private ReportKneeAngle kneeAngleScript;
-    private float lastSlider = 0f;
+    private float lastRot = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,29 +20,9 @@ public class MoveKneeJoint : MonoBehaviour
     }
 
     void FixedUpdate()
-    {
-        // this.transform.Rotate(this.kneeAngleScript.currentAngle, 0f, 0f, Space.Self);
-        // this.transform.eulerAngles = new Vector3(
-        //     this.kneeAngleScript.currentAngle,
-        //     this.transform.eulerAngles.y,
-        //     this.transform.eulerAngles.z
-        // );
-        
-        Quaternion localRotation = Quaternion.Euler(this.kneeAngleScript.currentAngle - lastSlider, 0f, 0f);
+    {      
+        Quaternion localRotation = Quaternion.Euler(this.kneeAngleScript.currentAngle - lastRot, 0f, 0f);
         this.transform.rotation = this.transform.rotation * localRotation;
-        lastSlider = this.kneeAngleScript.currentAngle;
-        // Vector3 eulers = this.transform.rotation.eulerAngles;
-        // this.transform.rotation = Quaternion.Euler(new Vector3(
-        //     this.kneeAngleScript.currentAngle - lastSlider,
-        //     eulers.y,
-        //     eulers.z
-        // ));
-        // lastSlider = this.kneeAngleScript.currentAngle;
-        // max = 180
-        // this.transform.rotation = Quaternion.Euler(-(this.kneeAngleScript.currentAngle + 180),this.rotation.,0);
-        // Debug.Log(this.transform.rotation);
-
-        // Vector3 targetRot = new Vector3(this.kneeAngleScript.currentAngle, this.transform.rotation.y, this.transform.rotation.z);
-        // transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 10);
+        lastRot = this.kneeAngleScript.currentAngle;
     }
 }
