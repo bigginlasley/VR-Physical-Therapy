89b11e3de764cc9ce7e7f6fecc3f6536d5809820
Jeremy Young <young.a.jeremy@gmail.com>
2022-04-22 20:39:58 -0600

    Menu works in MasterScene

--- PhysicalTherapy-Soccer/Assets/SceneHandler.cs
+++ PhysicalTherapy-Soccer/Assets/SceneHandler.cs
@@ -10,6 +10,9 @@ public class SceneHandler : MonoBehaviour
 {
     public SteamVR_LaserPointer laserPointer;
 
+    public GameObject MainMenu;
+    public GameObject CalibrationMenu;
+
     void Awake()
     {
         laserPointer.PointerIn += PointerInside;
@@ -19,41 +22,38 @@ public class SceneHandler : MonoBehaviour
 
     public void PointerClick(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "StartButton")
+        if (e.target.name == "CalibrateButton")
         {
-            Debug.Log("Start Button was clicked");
-        } else if (e.target.name == "Button")
-        {
-            Debug.Log("Button was clicked");
+            Debug.Log("Entered Calibration");
+            MainMenu.SetActive(false);
+            CalibrationMenu.SetActive(true);
         }
-
-         if (e.target.name == "ExitButton")
+        else if (e.target.name == "ExitButton")
         {
-            Debug.Log("Exit Button was clicked");
-        } else if (e.target.name == "Button")
-        {
-            Debug.Log("Button was clicked");
+            Debug.Log("Exited Calibration");
+            CalibrationMenu.SetActive(false);
+            MainMenu.SetActive(true);
         }
     }
 
     public void PointerInside(object sender, PointerEventArgs e)
     {
-        // if (e.target.name == "StartButton")
-        // {
-        //     Debug.Log("Start Button was entered");
-        // }
-        // else if (e.target.name == "Button")
-        // {
-        //     Debug.Log("Button was entered");
-        // }
+        if (e.target.name == "CalibrateButton")
+        {
+            Debug.Log("CalibrateButton Button was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
             
-        //     if (e.target.name == "ExitButton")
-        // {
-        //     Debug.Log("Exit Button was clicked");
-        // } else if (e.target.name == "Button")
-        // {
-        //     Debug.Log("Button was clicked");
-        // }
+            if (e.target.name == "PlayButton")
+        {
+            Debug.Log("PlayButton Button was clicked");
+        } else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+        }
     }
 
     public void PointerOutside(object sender, PointerEventArgs e)
