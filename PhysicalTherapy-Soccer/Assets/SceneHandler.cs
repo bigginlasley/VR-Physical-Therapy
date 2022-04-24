41d1179fb4ae44c69394e8eec5a68e7fa68444de
Jeremy Young <young.a.jeremy@gmail.com>
2022-04-23 18:48:10 -0600

    Clean up SceneHandler script and add all necessary frames

--- PhysicalTherapy-Soccer/Assets/SceneHandler.cs
+++ PhysicalTherapy-Soccer/Assets/SceneHandler.cs
@@ -12,6 +12,7 @@ public class SceneHandler : MonoBehaviour
 
     public GameObject MainMenu;
     public GameObject CalibrationMenu;
+    public GameObject PlayMenu;
 
     void Awake()
     {
@@ -20,59 +21,50 @@ public class SceneHandler : MonoBehaviour
         laserPointer.PointerClick += PointerClick;
     }
 
+    void Start() {
+        // Start with the MainMenu frame visible
+        MainMenu.SetActive(true);
+        CalibrationMenu.SetActive(false);
+        PlayMenu.SetActive(false);    
+    }
+
     public void PointerClick(object sender, PointerEventArgs e)
     {
+        // Enter calibration frame
         if (e.target.name == "CalibrateButton")
         {
             Debug.Log("Entered Calibration");
             MainMenu.SetActive(false);
             CalibrationMenu.SetActive(true);
         }
-        else if (e.target.name == "ExitButton")
+        // Exit calibration frame
+        else if (e.target.name == "LeaveCalibrationButton")
         {
             Debug.Log("Exited Calibration");
             CalibrationMenu.SetActive(false);
+            MainMenu.SetActive(true);
+        }
+        // Enter play frame
+        else if (e.target.name == "PlayButton")
+        {
+            Debug.Log("Entered Game");
+            MainMenu.SetActive(false);
+            PlayMenu.SetActive(true);
+        }
+        // Exit play frame
+        else if (e.target.name == "LeavePlayButton")
+        {
+            Debug.Log("Exited Game");
+            PlayMenu.SetActive(false);
             MainMenu.SetActive(true);
         }
     }
 
     public void PointerInside(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "CalibrateButton")
-        {
-            Debug.Log("CalibrateButton Button was entered");
-        }
-        else if (e.target.name == "Button")
-        {
-            Debug.Log("Button was entered");
-        }
-            
-            if (e.target.name == "PlayButton")
-        {
-            Debug.Log("PlayButton Button was clicked");
-        } else if (e.target.name == "Button")
-        {
-            Debug.Log("Button was clicked");
-        }
     }
 
     public void PointerOutside(object sender, PointerEventArgs e)
     {
-        // if (e.target.name == "StartButton")
-        // {
-        //     Debug.Log("Start Button was exited");
-        // }
-        // else if (e.target.name == "Button")
-        // {
-        //     Debug.Log("Button was exited");
-        // }
-
-        // if (e.target.name == "ExitButton")
-        // {
-        //     Debug.Log("Exit Button was clicked");
-        // } else if (e.target.name == "Button")
-        // {
-        //     Debug.Log("Button was clicked");
-        // }
     }
 }
